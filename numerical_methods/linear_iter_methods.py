a9928a8de1558ec3c14fbb4e87276a710dcbfdb2
Ethan He <yfhe@ucdavis.edu>
2023-02-15 14:09:26 -0800

    fix: type of gim, return #iter optional

--- numerical_methods/linear_iter_methods.py
+++ numerical_methods/linear_iter_methods.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy.linalg import norm
-from typing import Tuple, List, Callable
+from typing import Tuple, List, Callable, Union
 
 
 def _converged(curr: np.ndarray, prev: np.ndarray, thresh: float) -> bool:
@@ -8,14 +8,30 @@ def _converged(curr: np.ndarray, prev: np.ndarray, thresh: float) -> bool:
 
 
 def general_iter_method(
-    succ: Callable[[np.matrix], np.matrix], x0: np.ndarray, max_iter: int, thresh: float
-) -> Tuple[np.ndarray, int]:
-    for it in range(max_iter):
+    succ: Callable[[np.ndarray], np.ndarray],
+    x0: np.ndarray,
+    max_iter: int,
+    thresh: float,
+    return_iter: bool,
+) -> Union[Tuple[np.ndarray, int], np.ndarray]:
+    """General Iterative Method for linear systems
+
+    Args:
+        succ (Callable[[np.ndarray], np.ndarray]): compute the next approximation based on current solution
+        x0 (np.ndarray): initial guess
+        max_iter (int): maximum allowed iteration
+        thresh (float): convergence threshold
+        return_iter (bool): if True, return #iteration with the solution
+
+    Returns:
+        Union[Tuple[np.ndarray, int], np.ndarray]: solution w/o #iteration
+    """
+    for it in range(max_iter + 1):
         x = succ(x0)
         if _converged(x, x0, thresh):
-            return x, it
+            return (x, it) if return_iter else x
         x0, x = x, x0
-    return x, max_iter
+    return (x, max_iter) if return_iter else x
 
 
 def jacobi(
@@ -24,14 +40,15 @@ def jacobi(
     x0: np.ndarray,
     max_iter: int = 10,
     thresh: float = 1e-8,
-) -> Tuple[np.ndarray, int]:
+    return_iter: bool = False,
+):
     def succ(x0):
         x = np.zeros(shape=x0.shape)
         for i in range(x0.shape[0]):
             x[i] = (-A[i, :i] @ x0[:i] - A[i, i + 1 :] @ x0[i + 1 :] + b[i]) / A[i, i]
         return x
 
-    return general_iter_method(succ, x0, max_iter, thresh)
+    return general_iter_method(succ, x0, max_iter, thresh, return_iter)
 
 
 def gauss_seidel(
@@ -40,11 +57,12 @@ def gauss_seidel(
     x0: np.ndarray,
     max_iter: int = 10,
     thresh: float = 1e-8,
-) -> Tuple[np.ndarray, int]:
+    return_iter: bool = False,
+):
     def succ(x0):
         x = np.zeros(shape=x0.shape)
         for i in range(x0.shape[0]):
             x[i] = (-A[i, :i] @ x[:i] - A[i, i + 1 :] @ x0[i + 1 :] + b[i]) / A[i, i]
         return x
 
-    return general_iter_method(succ, x0, max_iter, thresh)
+    return general_iter_method(succ, x0, max_iter, thresh, return_iter)
