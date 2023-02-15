308a3baffeea3897ba7984affa14be6e3315d390
Ethan He <yfhe@ucdavis.edu>
2023-02-15 14:19:55 -0800

    feat: add sor

--- tests/linear_iter_methods_test.py
+++ tests/linear_iter_methods_test.py
@@ -1,7 +1,7 @@
 import unittest
 import numpy as np
 
-from numerical_methods.linear_iter_methods import jacobi, gauss_seidel
+from numerical_methods.linear_iter_methods import jacobi, gauss_seidel, sor
 
 
 class TestRoot1Var(unittest.TestCase):
@@ -10,7 +10,7 @@ class TestRoot1Var(unittest.TestCase):
         b = np.array([[6, 25, -11, 15]]).T
         x0 = np.zeros(shape=b.shape)
 
-        x, _ = jacobi(A, b, x0, max_iter=20)
+        x = jacobi(A, b, x0, max_iter=20)
         self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
 
     def test_gauss_seidel(self):
@@ -18,7 +18,15 @@ class TestRoot1Var(unittest.TestCase):
         b = np.array([[6, 25, -11, 15]]).T
         x0 = np.zeros(shape=b.shape)
 
-        x, _ = gauss_seidel(A, b, x0, max_iter=20)
+        x = gauss_seidel(A, b, x0, max_iter=20)
+        self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
+
+    def test_sor(self):
+        A = np.array([[10, -1, 2, 0], [-1, 11, -1, 3], [2, -1, 10, -1], [0, 3, -1, 8]])
+        b = np.array([[6, 25, -11, 15]]).T
+        x0 = np.zeros(shape=b.shape)
+
+        x = sor(A, b, x0, max_iter=20)
         self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
 
 
