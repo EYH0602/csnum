4920958cc3728d06ac6ade4328198e302686d554
Ethan He <yfhe@ucdavis.edu>
2023-02-15 14:21:01 -0800

    feat: randomized testing

--- tests/linear_iter_methods_test.py
+++ tests/linear_iter_methods_test.py
@@ -4,30 +4,36 @@ import numpy as np
 from numerical_methods.linear_iter_methods import jacobi, gauss_seidel, sor
 
 
+def generate_random(n):
+    A = n / 2 * np.identity(n) + np.random.normal(size=(n, n))
+    b = np.random.normal(size=(n, 1))
+    return A, b
+
+
 class TestRoot1Var(unittest.TestCase):
     def test_jacobi(self):
-        A = np.array([[10, -1, 2, 0], [-1, 11, -1, 3], [2, -1, 10, -1], [0, 3, -1, 8]])
-        b = np.array([[6, 25, -11, 15]]).T
-        x0 = np.zeros(shape=b.shape)
+        for _ in range(10):
+            A, b = generate_random(50)
+            x0 = np.zeros(shape=b.shape)
 
-        x = jacobi(A, b, x0, max_iter=20)
-        self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
+            x = jacobi(A, b, x0, max_iter=20)
+            self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
 
     def test_gauss_seidel(self):
-        A = np.array([[10, -1, 2, 0], [-1, 11, -1, 3], [2, -1, 10, -1], [0, 3, -1, 8]])
-        b = np.array([[6, 25, -11, 15]]).T
-        x0 = np.zeros(shape=b.shape)
+        for _ in range(10):
+            A, b = generate_random(50)
+            x0 = np.zeros(shape=b.shape)
 
-        x = gauss_seidel(A, b, x0, max_iter=20)
-        self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
+            x = gauss_seidel(A, b, x0, max_iter=20)
+            self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
 
     def test_sor(self):
-        A = np.array([[10, -1, 2, 0], [-1, 11, -1, 3], [2, -1, 10, -1], [0, 3, -1, 8]])
-        b = np.array([[6, 25, -11, 15]]).T
-        x0 = np.zeros(shape=b.shape)
+        for _ in range(10):
+            A, b = generate_random(50)
+            x0 = np.zeros(shape=b.shape)
 
-        x = sor(A, b, x0, max_iter=20)
-        self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
+            x = sor(A, b, x0, max_iter=20)
+            self.assertTrue(np.allclose(np.linalg.solve(A, b), x))
 
 
 if __name__ == "__main__":
