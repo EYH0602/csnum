b631ad5f199198ef449898899086708f40d3daf1
Ethan He <yfhe@ucdavis.edu>
2023-03-13 13:05:18 -0700

    feat: fix fixed_point arguments

--- tests/nonlinear_system_test.py
+++ tests/nonlinear_system_test.py
@@ -4,22 +4,17 @@ import numpy as np
 from numerical_methods.nonlinear_system import fixed_point
 
 
-def g1(xs):
-    x1, x2 = xs
-    return (x1**2 + x2**2 + 8) / 10
-
-
-def g2(xs):
-    x1, x2 = xs
-    return (x1 * x2**2 + x1 + 8) / 10
-
-
-G = [g1, g2]
-
-
 class TestNonlinearSystem(unittest.TestCase):
     def test_fixed_point(self):
-        sol = fixed_point(G, [0, 0], tol=1e-6)[-1]
+        def g1(xs):
+            x1, x2 = xs
+            return (x1**2 + x2**2 + 8) / 10
+
+        def g2(xs):
+            x1, x2 = xs
+            return (x1 * x2**2 + x1 + 8) / 10
+
+        sol = fixed_point(None, [0, 0], tol=1e-6, gs=[g1, g2])[-1]
         self.assertTrue(np.allclose(np.array(sol), np.ones(shape=(2,)), atol=1e-4))
 
 
