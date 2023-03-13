a6ffdf67a71c4a07c3c320ac4f08e4233c1a348f
Ethan He <yfhe@ucdavis.edu>
2023-03-13 13:49:10 -0700

    feat: jacobian matrix of NCts type

--- numerical_methods/nonlinear_system.py
+++ numerical_methods/nonlinear_system.py
@@ -1,11 +1,14 @@
 import jax.numpy as jnp
 from jax import grad
 import numpy as np
-from typing import Callable, List, Optional
+from typing import Callable, List, Optional, Iterable
+from inspect import signature
+
 from numerical_methods.root_finding_1_var import general_iter_method
 
 
 # G : R^n -> R^n
+# fix: f(x1, x2, ...) instead of f([x1, x2, ...])
 NCts = Callable[[List[float]], List[float]]  # not true, but works for know
 
 
@@ -16,7 +19,7 @@ def _converged(xs, ys, tol):
 
 
 def _ndapply(fs: NCts, xs: List[float]) -> List[float]:
-    return [f(xs) for f in fs]
+    return [f(*xs) for f in fs]
 
 
 def fixed_point(
@@ -36,4 +39,13 @@ def fixed_point(
     )
 
 
-# def newton()
+def jacobian(fs: NCts) -> List[NCts]:
+    J = []
+    for f in fs:
+        sig = signature(f)
+        J.append([grad(f, i) for i in range(len(sig.parameters))])
+    return J
+
+
+def jacobian_apply(J: List[NCts], xs: Iterable[float]) -> np.matrix:
+    return jnp.array([[f(*xs) for f in row] for row in J])
