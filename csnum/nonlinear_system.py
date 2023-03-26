8f85b4c51ea991ba3b07785daaeb2d4864802de3
Ethan He <yfhe@ucdavis.edu>
2023-03-25 20:42:04 -0700

    fix: type for nonlinear solvers

--- csnum/nonlinear_system.py
+++ csnum/nonlinear_system.py
@@ -1,15 +1,17 @@
 import jax.numpy as jnp
 from jax import grad
 import numpy as np
-from typing import Callable, List, Optional, Iterable
+from typing import Callable, List, Optional, Iterable, TypeVar, Any
 from inspect import signature
 
 from csnum.nonlinear_eqn import general_iter_method
 
 
 # G : R^n -> R^n
-# fix: f(x1, x2, ...) instead of f([x1, x2, ...])
-NCts = Callable[[List[float]], List[float]]  # not true, but works for know
+# ! using this to denote f(x1, x2, ..., xn) for x_i in float
+# ! if anyone knows a better type notation, please let me know
+NxFloat = TypeVar("NxFloat", List[float], np.ndarray, jnp.ndarray)
+NCts = Callable[[NxFloat], float]  # not true, but works for know
 
 
 def _converged(xs, ys, tol):
@@ -18,18 +20,21 @@ def _converged(xs, ys, tol):
     return np.linalg.norm(xs - ys) < tol
 
 
-def _ndapply(fs: NCts, xs: List[float]) -> List[float]:
-    return jnp.array([f(*xs) for f in fs])
+def _ndapply(fs: List[NCts], xs: List[float]) -> List[float]:
+    return [f(*xs) for f in fs]
 
 
 def fixed_point(
-    fs: NCts, p0: List[float], tol=1e-4, max_iter=15, gs: Optional[NCts] = None
+    fs: List[NCts],
+    p0: List[float],
+    tol=1e-4,
+    max_iter=15,
+    gs: Optional[List[NCts]] = None,
 ):
-    if gs is None:
-        gs = [lambda x: x - f(x) for f in fs]
+    succ: List[NCts] = gs if gs else [lambda x: x - f(x) for f in fs]
 
     return general_iter_method(
-        lambda ps: _ndapply(gs, ps[-1]),
+        lambda ps: _ndapply(succ, ps[-1]),
         p0,
         tol,
         max_iter,
@@ -39,7 +44,7 @@ def fixed_point(
     )
 
 
-def jacobian(fs: NCts) -> List[NCts]:
+def jacobian(fs: List[NCts]) -> List[List[NCts]]:
     J = []
     for f in fs:
         sig = signature(f)
@@ -47,17 +52,22 @@ def jacobian(fs: NCts) -> List[NCts]:
     return J
 
 
-def jacobian_apply(J: List[NCts], xs: Iterable[float]) -> np.matrix:
+def jacobian_apply(J: List[List[NCts]], xs: List[float]) -> jnp.ndarray:
     return jnp.array([_ndapply(fs, xs) for fs in J])
 
 
-def newton(fs: NCts, p0: Iterable[float], tol=1e-4, max_iter=15, Jf=None):
-    if Jf is None:
-        Jf = jacobian(fs)
+def newton(
+    fs: List[NCts],
+    p0: Iterable[float],
+    tol=1e-4,
+    max_iter=15,
+    Jf: Optional[List[List[NCts]]] = None,
+):
+    jacobian_mat = Jf if Jf else jacobian(fs)
 
     def succ(ps):
         xs = ps[-1]
-        J = jacobian_apply(Jf, xs)
+        J = jacobian_apply(jacobian_mat, xs)
         F = _ndapply(fs, xs)
         ys = jnp.linalg.solve(J, F)
         return xs - ys
